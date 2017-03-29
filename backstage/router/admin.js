796c062f84cf04f9cb1483f6ebfc2e416d67e8a6
zhouyong10 <1059981087@qq.com>
2017-03-29 20:46:39 +0800

    完成删除用户关联用户的子父级关系，并将被删除用户在执行的任务全部设置为已完成

--- backstage/router/admin.js
+++ backstage/router/admin.js
@@ -352,8 +352,20 @@ router.get('/user/edit/reset/password', function (req, res) {
 });
 
 router.get('/manage/user/del', function(req, res) {
-    User.removeUser(req.query.id).then(function () {
-        res.redirect('/admin/manage/user');
+    User.removeUser(req.query.id).then(function (username) {
+        Order.open().find({
+            user: username,
+            status: {$nin: ['已完成']}
+        }).then(function (orders) {
+                for (var i = 0; i < orders.length; i++) {
+                    Order.open().updateById(orders[i]._id, {
+                        $set: {
+                            status: '已完成'
+                        }
+                    });
+                }
+                res.redirect('/admin/manage/user');
+            });
     });
 });
 
