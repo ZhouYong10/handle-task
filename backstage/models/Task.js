aa1f0178657d862af3eb5df51506111920212d95
zhouyong10 <1059981087@qq.com>
2017-04-21 15:02:51 +0800

    修改任务自动审核时长为１２小时，调整所有任务下单页面提示信息

--- backstage/models/Task.js
+++ backstage/models/Task.js
@@ -595,7 +595,7 @@ function followedByPayment(tasks) {
         var task = tasks.shift();
         var taskCreateTime = task._id.getTimestamp();
         var timeNow = new Date().getTime();
-        if((timeNow - taskCreateTime) > 1000 * 60 * 60 * 24) {
+        if((timeNow - taskCreateTime) > 1000 * 60 * 60 * 12) {
             var taskIns = Task.wrapToInstance(task);
             taskIns.success().then(function () {
                 console.log(moment().format('YYYY-MM-DD HH:mm:ss') + ': 自动审核通过了任务' + task._id);
